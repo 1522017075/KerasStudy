b5143ac6611b0e1c32c4600f33fb028e4641846f
zhangziyangMac <ziyanglikezy@163.com>
2021-11-02 00:23:50 +0800

    线性回归入门以及数据集

---
+++ Keras_Lesson/2th手写识别demo优化.py
@@ -0,0 +1,35 @@
+import tensorflow.keras as keras
+# 科学计算
+import numpy as np
+# 画图
+import matplotlib.pyplot as plt
+# 层
+from tensorflow.keras import layers
+
+import tensorflow.keras.datasets.mnist as mnist
+
+(train_image, train_label), (test_image, test_label) = mnist.load_data()
+# 优化模型步骤1: 增大网络容量, 直到过拟合
+model = keras.Sequential()
+model.add(layers.Flatten())
+model.add(layers.Dense(64, activation='relu'))
+model.add(layers.Dense(64, activation='relu'))
+model.add(layers.Dense(64, activation='relu'))
+model.add(layers.Dense(10, activation='softmax'))
+
+# 编译模型
+#   优化函数: adam
+#   损失函数: sparse_categorical_crossentropy
+#   评价函数: 用于评估当前训练模型的性能
+model.compile(
+    optimizer='adam',
+    loss='sparse_categorical_crossentropy',
+    metrics=['acc']
+)
+
+# 训练模型(一个批次512张图, 训练50次) validation_data查看训练过程中测试数据的表现
+model.fit(train_image, train_label, epochs=50, batch_size=512, validation_data=(test_image, test_label))
+
+# 增大网络容量后的测试模型 loss: 0.2418 - acc: 0.9646
+model.evaluate(test_image, test_label)
+
---
+++ Keras_Lesson/6th线性回归.py
@@ -0,0 +1,24 @@
+import tensorflow.keras as keras
+import pandas as pd
+from tensorflow.keras import layers
+data = pd.read_csv('../sources/Advertising.csv')
+# print(data.head())
+x = data[data.columns[1:-1]]
+# iloc取值
+y = data.iloc[:, -1];
+
+model = keras.Sequential()
+# y_pred = w1 * x1 + w2 * x2 + w3 * x3 + bias
+model.add(layers.Dense(1, input_dim=3))
+
+# 回归问题的损失函数: 均方差
+model.compile(
+    optimizer='adam',
+    loss='mse'
+)
+
+model.fit(x, y, epochs=2000)
+
+# 因为训练用的是DataFrame
+predict_y = model.predict(pd.DataFrame([[300, 0, 0]]))
+print(predict_y) # [[16.523697]]
